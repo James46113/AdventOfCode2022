e262f736f68a2573abd0920c550fe67b09e35dd3
James46113 <jamescaroe@gmail.com>
2022-12-10 11:46:28 +0000

    Completed day#4

--- Day#4/Day#4.py
+++ Day#4/Day#4.py
@@ -1,15 +1,13 @@
 pairs = [[list(range(int(pair[0].split("-")[0]), int(pair[0].split("-")[1])+1)), list(range(int(pair[1].split("-")[0]), int(pair[1].split("-")[1])+1))] for pair in [line.strip().split(",") for line in open("Day#4input.txt", "r").readlines()]]
-#pairs = [line.strip().split(',') for line in open('Dec4input.txt', 'r').readlines()]
+
 num = 0
 num2 = 0
 cont = False
-#print(len(pairs))
-#print(pairs)
+
 for pair in pairs:
 	a = pair[0]
 	b = pair[1]
-#	b = [1, 2, 3, 4]
-	#a = [2, 3]
+
 	a_in_b = True
 	b_in_a = True
 	for numa in a:
@@ -31,5 +29,4 @@ for pair in pairs:
 	if b_in_a or a_in_b:
 		num += 1
 	
-	#print(num, pairs.index(pair))		
 print(num, num2)
