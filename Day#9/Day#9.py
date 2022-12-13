c4a59b698ae88cf665204bc60ff80fb2b749fef2
James Caroe <83098125+James46113@users.noreply.github.com>
2022-12-13 13:36:25 +0000

    Started Day#9

--- Day#9/Day#9.py
+++ Day#9/Day#9.py
@@ -4,15 +4,54 @@ hy = 0
 tx = 0
 ty = 0
 moves = [[character.strip() for character in inst.split(" ")] for inst in open("Day#9input.txt", "r").readlines()]
+
+def move_tail():
+    global hx, hy, tx, ty
+    near = False
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if not (x == 0 and y == 0):
+                if tx + x == hx and ty + y == hy:
+                    near = True
+                    break
+        if near:
+            break
+    print(near)
+    if not near:
+        print(hx-tx)
+        if hx - tx == -2 and hy - ty == 1:
+            tx -= 1
+            ty += 1
+        if hx - tx == -1 and hy - ty == 2:
+
+        elif hx - tx == 2:
+            tx += 1
+        elif hx - tx == -2:
+            tx -= 1
+        elif hy - ty == 2:
+            ty += 1
+        elif hy - ty == -2:
+            ty -= 1
+
+
+
 for move in moves:
     if move[0] == "U":
-        hy += int(move[1])
+        for _ in range(int(move[1])):
+            hy += 1
+            move_tail()
     elif move[0] == "D":
-        hy -= int(move[1])
+        for _ in range(int(move[1])):
+            hy -= 1
+            move_tail()
     elif move[0] == "L":
-        hx -= int(move[1])
+        for _ in range(int(move[1])):
+            hx -= 1
+            move_tail()
     elif move[0] == "R":
-        hx += int(move[1])
+        for _ in range(int(move[1])):
+            hx += 1
+            move_tail()
     if -1 > hx - tx:
         tx -= 1
     elif hx - tx > 1:
