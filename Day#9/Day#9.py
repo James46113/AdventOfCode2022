caaae20affa14b663b6278c080b400809d1dd4b2
James46113 <jamescaroe@gmail.com>
2022-12-13 21:33:54 +0000

    working on day#9

--- Day#9/Day#9.py
+++ Day#9/Day#9.py
@@ -16,14 +16,19 @@ def move_tail():
                     break
         if near:
             break
-    print(near)
     if not near:
-        print(hx-tx)
-        if hx - tx == -2 and hy - ty == 1:
+        if (hx - tx == -2 and hy - ty == 1) or (hx - tx == -1 and hy - ty == 2):
             tx -= 1
             ty += 1
-        if hx - tx == -1 and hy - ty == 2:
-
+        elif (hx - tx == 2 and hy - ty == 1) or (hx - tx == 1 and hy - ty == 2):
+            tx += 1
+            ty += 1
+        elif (hx - tx == -2 and hy - ty == -1) or (hx - tx == -1 and hy - ty == -2):
+            tx -= 1
+            ty -= 1
+        elif (hx - tx == 2 and hy - ty == -1) or (hy - ty == 1 and hy - ty == -2):
+            tx += 1
+            ty -= 1
         elif hx - tx == 2:
             tx += 1
         elif hx - tx == -2:
@@ -32,8 +37,18 @@ def move_tail():
             ty += 1
         elif hy - ty == -2:
             ty -= 1
+    if (tx, ty) not in visited:
+        visited.append((tx, ty))
+    return [tx, ty]
 
+test_moves = [["R", "4"], ["U", "4"], ["L", "3"], ["D", "1"]]
+hx += 2
+assert move_tail() == [1, 0]
+hy += 2
+assert move_tail() == [2, 1]
 
+hx = 0
+hy = 0
 
 for move in moves:
     if move[0] == "U":
@@ -57,4 +72,8 @@ for move in moves:
     elif hx - tx > 1:
         tx += 1
     print(move, hx, hy, tx, ty)
-    input()
+print(len(visited))
+for vis in visited:
+    if visited.count(vis) > 1:
+        print("bad")
+print(visited)
